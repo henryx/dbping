a7c6182eae95cc3f73fa4cb535141c5a41e743bc
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-07-20 15:35:03 +0200

    Added main class Added Maven POM

---
+++ src/main/java/com/application/dbping/Main.java
@@ -0,0 +1,15 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       dbping
+ Description   A ping like database tester
+  License       GPL version 2 (see LICENSE for details)
+ */
+
+package com.application.dbping;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
