e514eac4881a7ddc1fef2ecefa6f5412716265d5
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-07-21 17:40:13 +0200

    Checked authorizations info in command line

--- src/main/java/com/application/dbping/Main.java
+++ src/main/java/com/application/dbping/Main.java
@@ -10,6 +10,10 @@ import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.Namespace;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -33,9 +37,48 @@ public class Main {
         return parser;
     }
 
+    private String[] getauth(String auth) throws IOException {
+        BufferedReader br;
+        Console console;
+        String[] data, result;
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        result = new String[2];
+        if (auth == null) {
+
+            System.out.print("Enter username: ");
+            result[0] = br.readLine();
+        } else {
+            data = auth.split(":");
+
+            result[0] = data[0];
+            if (data.length > 1) {
+                result[1] =data[1];
+            }
+        }
+
+        if (result[0].equals("")) {
+            throw new IOException("No user passed");
+        }
+
+        if (result[1] == null || result[1].equals("")) {
+            console = System.console();
+            System.out.print("Enter password: ");
+            try {
+                result[1] = String.valueOf(console.readPassword());
+            }catch (NullPointerException e) {
+                // Fallback to clear input, if console does not work
+                result[1] = br.readLine();
+            }
+        }
+
+        return result;
+    }
+
     private void go(String[] args) {
         Namespace arguments;
         String dburi;
+        String[] auth;
         URI uri;
 
         arguments = this.initargs().parseArgsOrFail(args);
@@ -44,6 +87,7 @@ public class Main {
 
         try {
             uri = new URI(dburi);
+            auth = getauth(uri.getUserInfo());
 
             if (uri.getScheme().equals("mysql")) {
                 //TODO: implement MySQL ping
@@ -56,6 +100,7 @@ public class Main {
         } catch (UnsupportedOperationException e) {
             System.err.println(e.getMessage());
             System.exit(1);
+        } catch (IOException e) {
         }
     }
 
