006eb7b81837571d6d898b56f5c25ed580dd72ec
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-07-21 16:58:29 +0200

    Improved abstract class

--- src/main/java/com/application/dbping/database/Database.java
+++ src/main/java/com/application/dbping/database/Database.java
@@ -1,18 +1,28 @@
+/*
+  Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+  Project       dbping
+  Description   A ping like database tester
+  License       GPL version 2 (see LICENSE for details)
+ */
 package com.application.dbping.database;
 
 import java.net.URI;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public abstract class Database {
-    private URI uri;
+    protected URI uri;
+    protected Connection conn;
 
     public Database(URI uri) {
         this.uri = uri;
     }
-
-    public URI getURI() {
-        return uri;
-    }
-    
     public abstract void ping(int count);
     public abstract void ping();
+
+    public void close() {
+        try {
+            this.conn.close();
+        } catch (SQLException e) {}
+    }
 }
