798c71156774c63e5770dd2f3896fe76c96b4c02
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-08-24 16:18:41 +0200

    Updated version

--- src/main/java/com/application/dbping/Main.java
+++ src/main/java/com/application/dbping/Main.java
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
 
     private ArgumentParser initargs() {
         ArgumentParser parser;
